f8c173ef788f9665b69eb374c9f895fa3aa5b4ba
demarkok <demarkok@gmail.com>
2019-03-31 02:29:55 +0300

    fix wall

--- Assets/Scripts/WallDestructionScript.cs
+++ Assets/Scripts/WallDestructionScript.cs
@@ -9,21 +9,27 @@ public class WallDestructionScript : MonoBehaviour
 
     void Update()
     {
-        
+//        Debug.Log("fsoo");
     }
+    
+    
     
     void OnTriggerEnter2D(Collider2D other)
     {   
-        if ((int)GameObject.Find("CharacterRobotBoy").GetComponent<TransformationScript>().currentState == 1)
-        {
-            Debug.Log("hello");
-            this.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-            Debug.Log("hello1");
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            this.gameObject.GetComponent<Collider>().isTrigger = false;
-        }
+        Debug.Log("foo");
+
+        
+        
+//        if ((int)GameObject.Find("CharacterRobotBoy").GetComponent<TransformationScript>().currentState == 1)
+//        {
+//            Debug.Log("hello");
+//            this.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+//            Debug.Log("hello1");
+//            Destroy(this.gameObject);
+//        }
+//        else
+//        {
+//            this.gameObject.GetComponent<Collider>().isTrigger = false;
+//        }
     }
 }
