a3eb21e4b114903fdcc684ab44ca6cb5a5aa1303
shavkunov <mv.shavkunov@yandex.ru>
2019-03-30 23:40:13 +0300

    pushed meta

---
+++ Assets/Scripts/BombControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BombControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 40, 50, 80, 30), "GAME OVER");
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 40, 300, 60, 30), "Retry?"))
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(0);
+        }
+    }
+}
