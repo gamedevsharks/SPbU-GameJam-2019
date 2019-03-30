9073ce2df556760b110f30eadc95e3e5bda49889
Ivan Veselov <veselov.iva@gmail.com>
2019-03-31 02:58:03 +0300

    Game over if all batteries are collected.

---
+++ Assets/Scripts/BatteriesNumberChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BatteriesNumberChecker : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        BatteryControl[] batteries = Object.FindObjectsOfType<BatteryControl>();
+
+        if (batteries.Length == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
