b359ab2a3dc24fa1993531f1c5ecce0222e01902
Ivan Veselov <veselov.iva@gmail.com>
2019-03-31 01:58:14 +0300

    Make robot go to nearest battery.

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets._2D;
 
+using Object = UnityEngine.Object;
+
 public class RobotController : MonoBehaviour
 {
     private PlatformerCharacter2D m_Character;
@@ -40,5 +42,48 @@ public class RobotController : MonoBehaviour
             m_Character.Move(-1f, false, false);
         }
         
+    }
+    
+    void OnEnable()
+    { 
+        BatteryControl nearestButtery = FindNearestBattery();
+        target = new Vector2(nearestButtery.transform.position.x, nearestButtery.transform.position.y);
+    }
+
+    
+    private static float Length(Vector3 v)
+    {
+        return Math.Abs(v.x) + Math.Abs(v.y) + Math.Abs(v.z);
+    }
+
+    /**
+     * Returns the nearest battery or null if there are no batteries.
+     */
+    private BatteryControl FindNearestBattery()
+    {
+        var robotPosition = this.gameObject.transform.position;
+
+        float optimalDist = Single.PositiveInfinity;
+        BatteryControl nearestBattery = null;
+            
+        BatteryControl[] batteries = Object.FindObjectsOfType<BatteryControl>();
+        foreach (BatteryControl battery in batteries)
+        {
+            var batteryPosition = battery.gameObject.transform.position;
+            var dist = Length(batteryPosition - robotPosition);
+            if (dist < optimalDist)
+            {
+                optimalDist = dist;
+                nearestBattery = battery;
+            }
+        }
+
+        if (nearestBattery != null)
+        {
+            Object.Destroy(nearestBattery.gameObject);   
+        }
+
+        return nearestBattery;
+
     }
 }
