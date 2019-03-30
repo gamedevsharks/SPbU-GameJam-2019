08227c8c0e0cc9d4d7397d025eaf4e7d09e93339
Ivan Veselov <veselov.iva@gmail.com>
2019-03-31 01:42:25 +0300

    Make robot follow target.

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -8,7 +9,8 @@ public class RobotController : MonoBehaviour
 {
     private PlatformerCharacter2D m_Character;
 
-
+    private Vector2 target = new Vector2(-5f, 0f);
+    
     private void Awake()
     {
         m_Character = GetComponent<PlatformerCharacter2D>();
@@ -22,6 +24,21 @@ public class RobotController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        m_Character.Move(1f, false, false);
+        float diff = transform.position.x - target.x;
+        if (Math.Abs(diff) < 0.1)
+        {
+            m_Character.Move(0f, false, false);
+            return;
+        }
+        
+        if (diff < 0)
+        {
+            m_Character.Move(1f, false, false);
+        }
+        else
+        {
+            m_Character.Move(-1f, false, false);
+        }
+        
     }
 }
