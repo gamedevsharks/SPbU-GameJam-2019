adfe9f5da0df04358d20d166b6ebe67a5f020785
shavkunov <mv.shavkunov@yandex.ru>
2019-03-30 22:46:33 +0300

    added ladder script

---
+++ Assets/Scripts/StairsScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsScript : MonoBehaviour
+{
+    public float speed = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+   
+            if (Input.GetKey(KeyCode.W))
+            {
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+ 
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+            }
+            else
+            {
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+        }
+    }
+}
