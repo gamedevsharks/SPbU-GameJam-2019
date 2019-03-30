a352f1577f54ee58a8195080ab7d12ac6ddd6290
shavkunov <mv.shavkunov@yandex.ru>
2019-03-31 02:55:30 +0300

    added boundaries

--- Assets/Scripts/StairsScript.cs
+++ Assets/Scripts/StairsScript.cs
@@ -17,16 +17,26 @@ public class StairsScript : MonoBehaviour
     {
         
     }
-    
-    void OnTriggerStay2D(Collider2D other)
+
+    public float prevGravity = -1000000;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
+        //prevGravity = other.GetComponent<Rigidbody2D>().gravityScale;
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //other.GetComponent<Rigidbody2D>().gravityScale = prevGravity;  
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {              
         if (other.gameObject.CompareTag("Player"))
         {
-   
             if (Input.GetKey(KeyCode.W))
             {
                 other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
- 
             }
             else if (Input.GetKey(KeyCode.S))
             {
